2be7f83eb911f7aaa158cc5072d67a0a287f7429
Fiachra Corcoran <fiachra.corcoran@est.tech>
2024-04-18 23:07:23 +0100

    Update serv.py

--- serv.py
+++ serv.py
@@ -1,20 +1,9 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import http.server
+import socketserver
 
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
 
-class Serv(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        if self.path == '/':
-            self.path = '/test.html'
-        try:
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
-
-
-httpd = HTTPServer(('localhost', 8080), Serv)
-httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
